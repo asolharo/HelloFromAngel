505d82013ccd271989b2ae55a1511900b5a482d7
CarlosArredondo <arr15004@byui.edu>
2021-05-20 18:35:25 -0600

    Chang so that my name is displayed

--- src/HelloFromAngel.java
+++ src/HelloFromAngel.java
@@ -3,5 +3,7 @@ class HelloFromAngel
     public static void main(String args[])
     {
         System.out.println("Hello, my name is Angel.");
+        System.out.println("Hello Angle, my name is Carlos.");
     }
+
 }
